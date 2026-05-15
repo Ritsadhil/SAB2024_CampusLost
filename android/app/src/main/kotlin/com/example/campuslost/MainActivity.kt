252132f96ae4aadb7e95442b03b9477e5224fca6
igas1234 <fabregasandro@gmail.com>
2026-05-15 17:53:22 +0700

    Fix Play Store release build configuration

--- android/app/src/main/kotlin/com/example/campuslost/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.campuslost
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/penacode/campuslost/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.penacode.campuslost
+
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity()
